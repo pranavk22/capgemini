94b1e91da82015e3fef9f0157ac51a82c613fcbb
pranavk22 <53094301+pranavk22@users.noreply.github.com>
2020-02-01 00:11:47 +0530

    Added

---
+++ JUnit-Demo/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee e= new Employee();
+		e.setId(101);
+		e.setName("Pranav");
+		System.out.println(e.getId()+" : "+e.getName());
+	}
+}
